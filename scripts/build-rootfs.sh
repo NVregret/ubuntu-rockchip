7e1867743bacc75b1d3718c6584238abeb003078
骷髅头 <lasstp5011@gmail.com>
2023-05-23 11:11:04 +0800

    Update build-rootfs.sh

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -296,7 +296,7 @@ elif [[ "${BOARD}" =~ rock5b ]]; then
     echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmiin-sound", ENV{SOUND_DESCRIPTION}="HDMI-In Audio"' >> ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
     echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-dp0-sound", ENV{SOUND_DESCRIPTION}="DP0 Audio"' >> ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
     echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-es8316-sound", ENV{SOUND_DESCRIPTION}="ES8316 Audio"' >> ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
-elif [[ "${BOARD}" =~ nanopir6c|nanopir6s ]]; then
+elif [[ "${BOARD}" =~ nanopir6c|nanopir6s|lubancat-4 ]]; then
     echo 'SUBSYSTEM=="sound", ENV{ID_PATH}=="platform-hdmi0-sound", ENV{SOUND_DESCRIPTION}="HDMI0 Audio"' > ${chroot_dir}/etc/udev/rules.d/90-naming-audios.rules
 fi
 
